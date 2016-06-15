78e976d0b057af2035da87ff334b5fdc332b18ce
Maria <maria.lapovsky@gmail.com>
2016-06-16 02:52:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,32 @@
-/**
- * Created by Asus on 09.06.2016.
- */
-
 import com.epam.collections.*;
 
+/**
+ * Main class
+ */
 public class Main {
     public static void main(String[] args) {
-        CustomTreeSet<Integer> lol = new CustomTreeSet<>();
-        lol.add(25);
-        lol.add(26);
-        lol.add(33);
-        lol.add(28);
-        lol.add(76);
-        lol.add(26);
-        lol.add(85);
-        lol.add(10);
-        lol.add(38);
-        lol.add(19);
-        System.out.println(lol.toString());
+        CustomTreeSet<Integer> treeSet = new CustomTreeSet<>();
+        treeSet.add(25);
+        treeSet.add(26);
+        treeSet.add(33);
+        treeSet.add(28);
+        treeSet.add(76);
+        treeSet.add(26);
+        treeSet.add(85);
+        treeSet.add(10);
+        treeSet.add(38);
+        treeSet.add(19);
+        System.out.println(treeSet.toString());
 
-        CustomLinkedList<Integer> omg = new CustomLinkedList<>();
-        omg.add(25);
-        omg.add(26);
-        omg.add(27);
-        omg.add(28);
-        omg.add(29);
-        omg.add(33);
-        omg.add(19);
-        lol.retainAll(omg);
-        System.out.println(lol.toString());
-
-        //Object[] omg = lol.toArray();
-        // for (Object o : omg)
-            // System.out.print(o + " ");
+        CustomLinkedList<Integer> list = new CustomLinkedList<>();
+        list.add(25);
+        list.add(26);
+        list.add(27);
+        list.add(28);
+        list.add(29);
+        list.add(33);
+        list.add(19);
+        treeSet.retainAll(list);
+        System.out.println(treeSet.toString());
     }
 }
